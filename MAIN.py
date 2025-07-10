5e0b7382e611a38316b45dec7714adfcfda0730b
arian <arian.sh92@gmail.com>
2025-07-10 16:56:47 +0330

    ADVNC-EZ

--- MAIN.py
+++ MAIN.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     inp = input()
     chars = eval(inp)
 
-    simple_solver = SimpleEquationCSP(chars)
-    print("جواب ساده:","\n", simple_solver.solve())
+    # simple_solver = SimpleEquationCSP(chars)
+    # print("جواب ساده:","\n", simple_solver.solve())
     
     advanced_solver = SmartEquationCSP(chars)
     print("جواب پیشرفته:", "\n", advanced_solver.solve())
